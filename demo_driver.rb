9270f673f382ad68054c073d04ba3ab3dcac5d72
Bilal Itani <bilalitani1@gmail.com>
2020-01-29 08:33:34 -0500

    Remove test log, add in_bounds? check to grid

--- demo_driver.rb
+++ demo_driver.rb
@@ -5,5 +5,4 @@ def drive
 
   demo_grid = Grid.new width: 10, height: 10
   puts demo_grid
-  puts demo_grid.is_alive 0, 0
 end
--- grid.rb
+++ grid.rb
@@ -24,4 +24,21 @@ class Grid
   def is_alive column, row
     @matrix[column][row] == :o
   end
+
+  def next_gen_status column, row
+
+  end
+
+  def should_die? column, row
+
+  end
+
+  def in_bounds? column, row
+    left_in_bounds = row >= 0
+    right_in_bounds = row < @matrix[0].length
+    top_in_bounds = column >= 0
+    bottom_in_bounds = column < @matrix.length
+
+    left_in_bounds && right_in_bounds && top_in_bounds && bottom_in_bounds
+  end
 end
