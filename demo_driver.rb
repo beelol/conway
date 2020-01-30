3e2ee979ee9f5a1416be4a20cc49d5330833137a
Bilal Itani <bilalitani1@gmail.com>
2020-01-30 10:06:51 -0500

    Reformat files

--- demo_driver.rb
+++ demo_driver.rb
@@ -1,10 +1,11 @@
-require "./grid"
+require './grid'
 
 def drive
-  puts "now starting with demo"
+  puts 'now starting with demo'
 
   demo_grid = Grid.new width: 10, height: 10
   puts demo_grid
 
-  puts demo_grid.set_num_live_neighbors_at_live_cell 2, 2
+  # puts demo_grid.set_num_live_neighbors_at_live_cell 2, 2
+  puts demo_grid.set_num_live_neighbors_at_live_cell *gets.chomp.split(',').map(&:to_i)
 end
--- grid.rb
+++ grid.rb
@@ -1,47 +1,41 @@
 NEIGHBOR_OFFSETS = [-1, 0, 1].repeated_permutation(2).to_a
-NEIGHBOR_OFFSETS.delete [0,0]
+NEIGHBOR_OFFSETS.delete    [0, 0]
 
 class Grid
-
-  def initialize opts={}
-    opts = {width: 5, height: 5}.merge(opts)
+  def initialize(opts = {})
+    opts = { width: 5, height: 5 }.merge(opts)
 
     # NEIGHBOR_OFFSETS.each {|coord| puts "[#{coord[0]}, #{coord[1]}]"}
 
     @num_live_neighbors_by_dead_cells = {}
     @num_live_neighbors_by_live_cells = {}
 
-    @matrix = [];
-    
+    @matrix = []
+
     opts[:height].times do |i|
       @matrix[i] = []
 
-      opts[:width].times do |j|
-        @matrix[i][j] = [:x, :o].sample      
-      end
+      opts[:width].times { |j| @matrix[i][j] = %i[x o].sample }
     end
   end
 
   def to_s
-    @matrix.inject("") do |combined_rows, row|
-      formatted_row = row.inject("") { |combined_cells, cell| "#{combined_cells} #{cell}" }
+    @matrix.inject('') do |combined_rows, row|
+      formatted_row =
+        row.inject('') { |combined_cells, cell| "#{combined_cells} #{cell}" }
       "#{combined_rows}\n#{formatted_row}"
     end
   end
-    
-  def is_alive row, column
+
+  def is_alive(row, column)
     @matrix[row][column] == :o
   end
 
-  def next_gen_status row, column
+  def next_gen_status(row, column); end
 
-  end
+  def should_die?(row, column); end
 
-  def should_die? row, column
-
-  end
-
-  def in_bounds? row, column
+  def in_bounds?(row, column)
     left_in_bounds = column >= 0
     right_in_bounds = column < @matrix[0].length
     top_in_bounds = row >= 0
@@ -55,7 +49,7 @@ class Grid
   # may come alive if necessary conditions are met.
 
   # make sure to CLEAR the hash before next generation is set to the matrix.
-  def set_num_live_neighbors_at_live_cell row, column
+  def set_num_live_neighbors_at_live_cell(row, column)
     return if !is_alive row, column
 
     NEIGHBOR_OFFSETS.each do |offset_factor|
@@ -65,7 +59,9 @@ class Grid
       if in_bounds? neighbor_row, neighbor_column
         if is_alive neighbor_row, neighbor_column
           add_live_cell_num_neighbors [row, column]
-          puts "[#{neighbor_row}, #{neighbor_column}] is a live neighbor to [#{row}, #{column}]"
+          puts "[#{neighbor_row}, #{neighbor_column}] is a live neighbor to [#{
+                 row
+               }, #{column}]"
         else
           add_dead_cell_num_neighbors [neighbor_row, neighbor_column]
           puts "[#{neighbor_row}, #{neighbor_column}] is in bounds"
@@ -78,7 +74,7 @@ class Grid
     @num_live_neighbors_by_live_cells[[row, column]]
   end
 
-  def add_dead_cell_num_neighbors position
+  def add_dead_cell_num_neighbors(position)
     if @num_live_neighbors_by_dead_cells.has_key? position
       @num_live_neighbors_by_dead_cells[position] += 1
     else
@@ -86,7 +82,7 @@ class Grid
     end
   end
 
-  def add_live_cell_num_neighbors position
+  def add_live_cell_num_neighbors(position)
     if @num_live_neighbors_by_live_cells.has_key? position
       @num_live_neighbors_by_live_cells[position] += 1
     else
